ee050ed2032cbb59e46178947a284ca5e7e49af7
vibhu gupta <vibhugupta022@gmail.com>
2026-06-02 00:10:05 +0530

    done

--- src/components/UpperNav.jsx
+++ src/components/UpperNav.jsx
@@ -28,7 +28,7 @@ export function UpperNavbar({ navbarRef }) {
   const menuToggleRef = useRef(null);
 
   const handleBookCall = () => {
-    window.open("https://cal.com/vibhu-gupta-d2rvc8/websync", "_blank", "noopener,noreferrer");
+    window.open("https://cal.com/websync.co.in/bookmeeting", "_blank", "noopener,noreferrer");
   };
 
   // Scroll to section + close menu
